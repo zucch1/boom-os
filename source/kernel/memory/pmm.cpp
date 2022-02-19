0a86f8fdeec32d9cb4a6ee633b3db04488dc29cf
Ambele Newton <newtonambele@gmail.com>
2022-02-19 11:19:17 +0100

    Fiexed bug: PMM reserve_frames PMM now reserves frames with no issues and memory can be requested and gotten. But: reserving the first 1MB (BIOS) region causes the entire pmm_bitmap to set to  all 1s for no know reasons

--- source/kernel/memory/pmm.cpp
+++ source/kernel/memory/pmm.cpp
@@ -1,11 +1,13 @@
-/* LICENSING INFO*/
-/* pmm description */
-// extern uint64_t _KernelStart;
+/* TODO:                    */
+/* add LICENSING INFO       */
+/* add pmm.cpp description  */
+/* modify *_frames to roll back all actions if any failure occurs at any point*/
+/* Reserve region below 1MB */
+/* Remove kernel.h */
 
 #include "pmm.h"
 #include "bitmap.h"
 #include "stivale2.h"
-#include "kernel.h"
 #include "util.h"
 
 static bitmap pmm_bitmap{};
@@ -22,13 +24,13 @@ bool lock_frame(void * address){
     if(!pmm_init) return false;
     uint64_t index = (uint64_t)address/4096;
     if(pmm_bitmap[index] == true) return true; 
-    pmm_bitmap.set(index, 1);
+    pmm_bitmap.set(index, true);
     FreeMemory -= 0x1000;
     UsedMemory += 0x1000;
     return true;
 }
 bool lock_frames(void * address, uint64_t n_frames){
-    for(int i = 0; i < n_frames; i++){
+    for(uint64_t i = 0; i < n_frames; i++){
         if(!lock_frame((void*)((uint64_t)address + (i*4096)))) return false;
     }
     return true;
@@ -38,14 +40,14 @@ bool lock_frames(void * address, uint64_t n_frames){
 bool free_frame(void * address){
     if(!pmm_init) return false;
     uint64_t index = (uint64_t)address/4096;
-    if(!pmm_bitmap[index]) return true; 
-    pmm_bitmap.set(index, 0);
+    if(pmm_bitmap[index] == false) return true; 
+    pmm_bitmap.set(index, false);
     FreeMemory += 0x1000;
     UsedMemory -= 0x1000;
     return true;
 }
 bool free_frames(void * address, uint64_t n_frames){
-    for(int i = 0; i < n_frames; i++){
+    for(uint64_t i = 0; i < n_frames; i++){
         if(!free_frame((void*)((uint64_t)address + (i*4096)))) return false;    // return false iff free_frame returned false at any moment
     }
     return true;
@@ -54,14 +56,14 @@ bool free_frames(void * address, uint64_t n_frames){
 bool reserve_frame(void * address){
     if(!pmm_init) return false;
     uint64_t index = (uint64_t)address/4096;
-    if(pmm_bitmap[index]) return true; 
-    pmm_bitmap.set(index, 1);
+    if(pmm_bitmap[index] == true) return true; 
+    pmm_bitmap.set(index, true);
     FreeMemory      -= 0x1000;
     ReservedMemory  += 0x1000;
     return true;
 }
 bool reserve_frames(void * address, uint64_t n_frames){
-    for(int i = 0; i < n_frames; i++){
+    for(uint64_t i = 0; i < n_frames; i++){
         if(!reserve_frame((void*)((uint64_t)address + (i*4096)))) return false;
     }
     return true;
@@ -70,14 +72,14 @@ bool reserve_frames(void * address, uint64_t n_frames){
 bool unreserve_frame(void * address){
     if(!pmm_init) return false;
     uint64_t index = (uint64_t)address/4096;
-    if(!pmm_bitmap[index]) return true; 
-    pmm_bitmap.set(index, 0);
+    if(pmm_bitmap[index] == false) return true; 
+    pmm_bitmap.set(index, false);
     FreeMemory      += 0x1000;
     ReservedMemory  -= 0x1000;
     return true;
 }
 bool unreserve_frames(void * address, uint64_t n_frames){
-    for(int i = 0; i < n_frames; i++){
+    for(uint64_t i = 0; i < n_frames; i++){
         if(!unreserve_frame((void*)((uint64_t)address + (i*4096)))) return false;
     }
     return true;
@@ -94,22 +96,26 @@ void init_pmm(stivale2_struct * bootinfo){
             largest_seg = mmap->memmap[i].base;
         }
     }
-    pmm_bitmap = bitmap((uint8_t*)largest_seg, get_memory_size(bootinfo)/4096 + 1);
-    memset(pmm_bitmap.buffer, 0, get_memory_size(bootinfo)/4096/8+1);   // Set the buffer to all 1s (reserve all pages)
     FreeMemory = get_memory_size(bootinfo);
-    lock_frames(pmm_bitmap.buffer, pmm_bitmap.n_bytes/0x1000);
+    pmm_bitmap = bitmap((uint8_t*)largest_seg, FreeMemory/4096 + 1);
+    memset(pmm_bitmap.buffer, 0, pmm_bitmap.n_bytes);   // Clear the buffer
+
+    lock_frames(pmm_bitmap.buffer, (pmm_bitmap.n_bytes/0x1000) + 1);
     for(uint16_t i = 0; i < mmap->entries; i++){
         if(mmap->memmap[i].type != USABLE){
             reserve_frames((void*)mmap->memmap[i].base, mmap->memmap[i].length/0x1000);
         } 
     }
-    
+    //######################################################################
+    // Lock BIOS region below 1MB (0x100000)
+    // reserve_frames((void*)0, 256);       //NOTE: This causes pmm_bug_all_ones, no know reason
 }
 //=-----------------------------------------------------------=
 
 // Searches linearly from last requested page;
 void * request_frame(){
-    for(uint64_t i = 0; i < pmm_bitmap.n_bytes*8; i++){
+    // Skip the first 1MB (BIOS Area) i.e 256 pages
+    for(uint64_t i = 256; i < pmm_bitmap.n_bytes*8; i++){
         uint64_t address = 0;
         if(pmm_bitmap[i] == false){
             address = i*4096;
@@ -125,7 +131,6 @@ void * request_frame(){
 bool frame_status(void * address){
     return pmm_bitmap[(uint64_t)address/4096];
 }
-
 
 size_t GetUsedMemory(){
     return UsedMemory;
